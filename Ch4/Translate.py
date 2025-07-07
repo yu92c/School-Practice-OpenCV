5a012401a3154c663b93a815708b3b0343a9756d
K.C <yu992k@gmail.com>
2025-07-07 16:50:32 +0800

    fix

--- Ch4/Translate.py
+++ Ch4/Translate.py
@@ -28,20 +28,4 @@ def translate(img, x, y):
 
 translated = translate(img, 100, 100)
 cv.imshow('Translated', translated)
-
-# 3. Rotation --------------------------------------------------------------------------------
-def rotate(img, angle, rotPoint=None):
-    (height, width) = img.shape[:2]
-
-    if rotPoint is None:
-       rotPoint = (width // 2, height // 2)
-
-    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
-    dimensions = (width, height)
-    return cv.warpAffine(img, rotMat, dimensions)
-
-rotated = rotate(img, 45)
-cv.imshow('Rotated', rotated)
-
 cv.waitKey(0)
-cv.destroyAllWindows()
