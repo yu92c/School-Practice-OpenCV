1b49fb42aa8b3f4107841471b548873acddf40e9
K.C <yu992k@gmail.com>
2025-07-09 23:03:22 +0800

    update

--- Ch2/read-img.py
+++ Ch2/read-img.py
@@ -4,9 +4,12 @@
 
 import cv2 as cv
 
-img = cv.imread('pic/cb.jpg')             # loading image.
+img = cv.imread('pic/cb.jpg')              # loading image.
+print(img.shape)
 cv.imshow('cb', img)                      # display image in new window ('cb' jsut title name).
 cv.waitKey(0)                             # will keep showing till press esc.
 
 # for now just using small image.
 # if load large image (larger than your screen),it might go off screen.
+
+
