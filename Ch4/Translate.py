6edefcfdfa7b13367c36d60eb3bd3ab3a2beb123
K.C <yu992k@gmail.com>
2025-07-11 03:00:04 +0800

    update

--- Ch4/Translate.py
+++ Ch4/Translate.py
@@ -1,23 +1,10 @@
-# 2D Affine Transformation in OpenCV 一律使用 2×3 矩陣，這是固定格式
-# 在 NumPy 或 Python 中，2D 矩陣的索引格式是 ： [ 行 ][ 列 ]
-# 因為：輸入是 (x, y, 1)，輸出是 (x', y')。 
-
-
-#| Affin Transformation Type   | Description                                                  
-#| ----------------------------| ------------------------------------------------------------ |
-#| 1. Translation              | Moves the image in X or Y direction                          |
-#| 2. Scaling                  | Enlarges or shrinks the image                                |
-#| 3. Rotation                 | Rotates the image around a point                             |
-#| 4. Shearing (Skewing)       | Slants the shape of the image in X or Y                      |
-#| 5. Reflection               | Mirrors the image across an axis                             |
-#| 6. Combination              | You can combine any of the above in one single affine matrix |
-
-# 1. translate--------------------------------------------------------------------------------
+# 仿射轉換（Affine Transformations）
+# 改變影像的位置、角度、大小、形狀，但會保持線性結構（也就是直線還是直線，平行還是平行）。
+# 平移	將圖像往上下左右移動
 
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('pic/cb.jpg')
 cv.imshow('Original', img)     
 
 
@@ -25,10 +12,16 @@ def translate(img, x, y):
     transMat = np.float32([[1, 0, x], [0, 1, y]])
     dimensions = (img.shape[1], img.shape[0])
     return cv.warpAffine(img, transMat, dimensions)
+    #仿射轉換 cv.warpAffine() + 矩陣
 
 translated = translate(img, 100, 100)
 cv.imshow('Translated', translated)
 cv.waitKey(0)
+
+# 2D Affine Transformation in OpenCV 一律使用 2×3 矩陣，這是固定格式
+# 在 NumPy 或 Python 中，2D 矩陣的索引格式是 ： [ 行 ][ 列 ]
+# 因為：輸入是 (x, y, 1)，輸出是 (x', y')。 
+
 
 #---------------------------------------------------------------------------------
 # 搬移座標：像素座標要用矩陣去計算新位置（用 warpAffine()）
---
+++ Ch4/shear.py
@@ -0,0 +1,57 @@
+# 仿射轉換（Affine Transformations）
+# Shear 偏移 。
+# 沿著 X 或 Y 軸「傾斜拉扯」，角度不變，長度不變 。
+# 四邊形傾斜成平行四邊形，像被橡皮筋拉了一樣 。
+# 不改物體內部角度，只改外框傾斜 。
+
+# 原始正方形（未變形）：
+#   +-----+
+#   |     |
+#   |     |
+#   +-----+
+
+
+#  Shear X（負數）,（向右傾）： 
+#  +---------+
+#  \  ABCDE  \
+#   \  ABCDE  \
+#    \  ABCDE  \
+#     +---------+
+
+# Shear X（正數）, （向左傾）：
+#      +---------+
+#     /  ABCDE  /
+#    /  ABCDE  /
+#   /  ABCDE  /
+#  +---------+
+
+
+
+import cv2
+import numpy as np
+
+# 讀取圖像
+img = cv2.imread('pic/cb.jpg')
+h, w = img.shape[:2] #只取高、寬（不含色彩通道）
+
+
+# ===== 水平切變（Shear X） =====
+shear_x = 0.5 # 正值：向右斜，負值：向左斜
+
+# 平移補償：讓圖像在畫布中央對齊
+lr = - shear_x * h / 2  # ❗ 負號，表示向左平移
+
+# === 仿射矩陣 M_x ===
+M_x = np.float32([
+    [1, shear_x, lr],  # x' = x + shear_x * y + lr
+    [0, 1,        0]   # y 不變
+])
+
+# === 輸出畫布寬度要加大，以容納傾斜後的圖像 ===
+out_w = int(w + abs(shear_x) * h)
+sheared_x = cv2.warpAffine(img, M_x, (out_w, h))
+
+# === 顯示結果 ===
+cv2.imshow('Original', img)
+cv2.imshow('Shear X（向右傾斜）', sheared_x)
+cv2.waitKey(0)
